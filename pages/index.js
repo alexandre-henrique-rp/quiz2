8354ad3cfee05f05822527fa0e8af5ae0c2104b6
Alexandre-herique <64115785+Alexandre-herique@users.noreply.github.com>
2021-01-25 23:17:37 -0300

    up

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,40 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from './src/components/Widget/index'
+import QuizLogo from './src/components/Qizlogo/index'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <QuizLogo>
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+          </Widget>
+        </QuizLogo>
+      </QuizContainer>
+    </BackgroundImage>
+  );
 }
