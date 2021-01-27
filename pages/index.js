e17a6db4833c0cbdd337454de44a2d8b1689b0b9
Alexandre-herique <64115785+Alexandre-herique@users.noreply.github.com>
2021-01-27 16:01:47 -0300

    parte 2

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import styled from 'styled-components'
-import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+/* eslint-disable func-names */
+import React from 'react';
 import Head from 'next/head';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
-
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -21,6 +23,8 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [nome, setNome] = React.useState('');
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
@@ -30,8 +34,8 @@ export default function Home() {
         <meta property="og:url" content="https://caverna-do-drago-quiz.alexandre-herique.vercel.app/" />
         <meta property="og:image" content="https://i.pinimg.com/originals/04/2f/4a/042f4af66eaca08be86f2bc87144f623.jpg" />
         <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="300" /> 
-        <meta property="og:image:height" content="300" /> 
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
         <title>Quiz - Caverna do Dragão</title>
       </Head>
       <QuizContainer>
@@ -42,6 +46,26 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={function (infosDoEventos) {
+              infosDoEventos.preventDefault();
+              router.push(`./quiz?nome=${nome}`);
+
+              // (`/quiz?name=${nome}`);
+            }}
+            >
+              {/* router manda para proxima pagina  */}
+              <input
+                onChange={function (infosDoEventos) {
+                  setNome(infosDoEventos.target.value);
+                }}
+                placeholder="Nickname"
+              />
+              <button type="submit" disabled={nome.length === 0}>
+                Bora jogar
+                {' '}
+                {nome}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
---
+++ pages/quiz.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function quiz() {
+  return (
+    <div>
+      oi
+    </div>
+  );
+}
